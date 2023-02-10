d9c4d068f39dfdbadf5973588c960b936924d244
DwAwilliams23 <120130350+DwAwilliams23@users.noreply.github.com>
2023-02-10 18:36:53 -0500

    Implementing full CRUD

--- main_app/models.py
+++ main_app/models.py
@@ -1,19 +1,37 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
 from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    favorite_color = models.CharField(max_length=50)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=250)
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     first_name = models.CharField(max_length=100)
+#     last_name = models.CharField(max_length=100)
+#     email = models.EmailField(max_length=250)
 
 class Hive(models.Model):
     title = models.CharField(max_length=250)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     location = models.CharField(max_length=250)
     description = models.TextField(max_length=250)
-    comments = models.TextField(max_length=250)
-    reviews = models.BooleanField()
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} ({self.id})'
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'hive_id': self.id})
+
+class Comment(models.Model):
+    date = models.DateField('comment date')
+    content = models.TextField(max_length=250)
+
+    hive = models.ForeignKey(
+        Hive,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.date}"
+
--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,9 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Hive
+from .forms import CommentForm
 
 # Create your views here.
 def home(request):
@@ -13,23 +15,47 @@ def about(request):
   return render(request, 'about.html')
 
 def hives_index(request):
-  return render(request, 'hives/index.html')
+  hives = Hive.objects.all()
+  return render(request, 'hives/index.html', {
+    'hives': hives
+  })
+
+def hives_detail(request, hive_id):
+  hive = Hive.objects.get(id=hive_id)
+  comment_form = CommentForm()
+  return render(request, 'hives/detail.html', { 'hive': hive, 'comment_form': comment_form
+  })
+
+def add_comment(request, hive_id):
+  form = CommentForm(request.POST)
+  if form.is_valid():
+    new_comment = form.save(commit=False)
+    new_comment.hive_id = hive_id
+    new_comment.save()
+  return redirect('detail', hive_id=hive_id) 
+
+class HiveCreate(CreateView):
+  model = Hive
+  fields = ['title', 'location', 'description']
+
+class HiveUpdate(UpdateView):
+  model = Hive
+  fields = ['title', 'location', 'description']
+
+class HiveDelete(DeleteView):
+  model = Hive
+  success_url = '/hives'
 
 def signup(request):
   error_message = ''
   if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
     form = UserCreationForm(request.POST)
     if form.is_valid():
-      # This will add the user to the database
       user = form.save()
-      # This is how we log a user in via code
       login(request, user)
       return redirect('home')
     else:
       error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
